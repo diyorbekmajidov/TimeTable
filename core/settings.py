caa67028ef7d903d65561c6f0862af3b3813405d
Diyorbek <majidovdiyorbek741@gmail.com>
2024-06-10 16:06:12 +0500

    cors error

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-d&liry+_*5kd@r3#lfblci_yzgj84bdu*4)u@^bmu#*p#83o8z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
