047729a93603d5447ef5366b63f7d7f974e3f40f
Diyorbek <majidovdiyorbek741@gmail.com>
2024-06-10 14:54:59 +0500

    add to static root

--- User/urls.py
+++ User/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import(
     RegisterUser,
@@ -13,3 +15,5 @@ urlpatterns = [
     path('user/logout/', UserLogoutAPIView.as_view()),
     path('user/', GetUserAPIView.as_view(), name='get_user'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
